6609a00f51d46d1e79038d07bd19a466ea55a910
Beckett Hamilton <hamiltr2@aston.ac.uk>
2021-07-19 10:49:06 +0100

    added some text

--- test.py
+++ test.py
@@ -9,13 +9,15 @@ class Window(Frame):
 
         #Our window should have an exit button at all times
         exitButton = Button(self, text="Quit", command=self.clickExitButton)
-        exitButton.place(x=0, y=0)
+        exitButton.place(x=900, y=160)
 
         #adding title and body
-        text1 = Label(self, text="Title", bg="#996Db6", font=("Helvetica", 25))
+        text1 = Label(self, text="Welcome!", bg="#996Db6", font=("Helvetica", 25))
         text1.place(x=70,y=90)
 
-        text2 = Label(self, text="blah blah blah", bg="#996Db6", font=("Helvetica",15))
+        text2 = Label(self,
+                      text="Welcome to My Threat Model. Please select your devices to get started.",
+                      bg="#996Db6", font=("Helvetica",15))
         text2.place(x=70,y=130)
 
     #Define what clicking the exit button does
@@ -27,8 +29,8 @@ root = Tk()
 app = Window(root)
 
 #naming the window and setting a size
-root.wm_title("Testing Text")
-root.geometry("320x200")
+root.wm_title("My Threat Model")
+root.geometry("1000x200")
 app['bg']='#996Db6'
 
 
