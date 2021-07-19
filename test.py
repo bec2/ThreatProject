149476d1c0f5157cd472416be45f86f6e6f4460c
Beckett Hamilton <hamiltr2@aston.ac.uk>
2021-07-20 00:27:49 +0100

    fixed stupid brackets mistake

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ class Window(Frame):
         Frame.__init__(self, master)
         self.master=master
         self.pack(fill=BOTH, expand=1)
-
+        
         #Our window should have an exit button at all times
         exitButton = Button(self, text="Quit", command=self.clickExitButton)
         exitButton.place(x=900, y=160)
@@ -23,11 +23,16 @@ class Window(Frame):
     #Define what clicking the exit button does
     def clickExitButton(self):
         exit()
-
-
+        
 #establishing a root window for the software
 root = Tk()
 app = Window(root)
+
+
+#naming the window and setting a size
+root.wm_title("My Threat Model")
+root.geometry("1000x500")
+app['bg']='#996Db6'
 
 #some tickboxes
 c=Checkbutton(root, text="Home Virtual Assistant (e.g. Alexa, Home)")
@@ -49,30 +54,27 @@ c6=Checkbutton(root, text="Smart fitness aids (e.g. SmartMat)")
 c6.pack()
 
 c7=Checkbutton(root, text="Smart kitchenwear")
-c7.pack
+c7.pack()
 
 c8=Checkbutton(root, text="Smart home security locks")
-c8.pack
+c8.pack()
 
 c9=Checkbutton(root, text="Amazon IoT Dash Button")
-c9.pack
+c9.pack()
 
 c10=Checkbutton(root, text="Smart thermostats and air monitors")
-c10.pack
+c10.pack()
 
 c11=Checkbutton(root, text="Automated 'smart home' controller")
-c11.pack
+c11.pack()
 
 c12=Checkbutton(root, text="Smart sleep tracker")
-c12.pack
+c12.pack()
 
 c13=Checkbutton(root, text="Any other smart home devices")
-c13.pack
+c13.pack()
 
-#naming the window and setting a size
-root.wm_title("My Threat Model")
-root.geometry("1000x200")
-app['bg']='#996Db6'
+
 
 #run
 root.mainloop()
