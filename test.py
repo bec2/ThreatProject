9c305178ccdce06836c98d771c656669e7aaf9dd
Beckett Hamilton <hamiltr2@aston.ac.uk>
2021-07-24 18:11:33 +0100

    holds status of risk boxes

--- test.py
+++ test.py
@@ -68,7 +68,6 @@ checkB14 = IntVar()
 def click1():
     global d1
     global check1
-
     if check1.get() :
         d1 = 1
     else:
@@ -77,7 +76,6 @@ def click1():
 def click2():
     global d2
     global check2
-
     if check2.get() :
         d2 = 1
     else:
@@ -86,7 +84,6 @@ def click2():
 def click3():
     global d3
     global check3
-
     if check3.get() :
         d3 = 1
     else:
@@ -95,7 +92,6 @@ def click3():
 def click4():
     global d4
     global check4
-
     if check4.get() :
         d4 = 1
     else:
@@ -104,7 +100,6 @@ def click4():
 def click5():
     global d5
     global check5
-
     if check5.get() :
         d5 = 1
     else:
@@ -113,7 +108,6 @@ def click5():
 def click6():
     global d6
     global check6
-
     if check6.get() :
         d6 = 1
     else:
@@ -122,7 +116,6 @@ def click6():
 def click7():
     global d7
     global check7
-
     if check7.get() :
         d7 = 1
     else:
@@ -131,7 +124,6 @@ def click7():
 def click8():
     global d8
     global check8
-
     if check8.get() :
         d8 = 1
     else:
@@ -140,7 +132,6 @@ def click8():
 def click9():
     global d9
     global check9
-
     if check9.get() :
         d9 = 1
     else:
@@ -149,7 +140,6 @@ def click9():
 def click10():
     global d10
     global check10
-
     if check10.get() :
         d10 = 1
     else:
@@ -158,7 +148,6 @@ def click10():
 def click11():
     global d11
     global check11
-
     if check11.get() :
         d11 = 1
     else:
@@ -167,7 +156,6 @@ def click11():
 def click12():
     global d12
     global check12
-
     if check12.get() :
         d12 = 1
     else:
@@ -176,11 +164,123 @@ def click12():
 def click13():
     global d13
     global check13
-
     if check13.get() :
         d13 = 1
     else:
         d13 = 0
+
+#same as above, but for risk factor variables
+def clickB1():
+    global r1
+    global checkB1
+    if checkB1.get() :
+        r1 = 1
+    else:
+        r1 = 0
+
+def clickB2():
+    global r2
+    global checkB2
+    if checkB2.get() :
+        r2 = 1
+    else:
+        r2 = 0
+
+def clickB3():
+    global r3
+    global checkB3
+    if checkB3.get() :
+        r3 = 1
+    else:
+        r3 = 0
+
+def clickB4():
+    global r4
+    global checkB4
+    if checkB4.get() :
+        r4 = 1
+    else:
+        r4 = 0
+
+def clickB5():
+    global r5
+    global checkB5
+    if checkB5.get() :
+        r5 = 1
+    else:
+        r5 = 0
+
+def clickB6():
+    global r6
+    global checkB6
+    if checkB5.get() :
+        r6 = 1
+    else:
+        r6 = 0
+
+def clickB7():
+    global r7
+    global checkB7
+    if checkB7.get() :
+        r7 = 1
+    else:
+        r7 = 0
+
+def clickB8():
+    global r8
+    global checkB8
+    if checkB8.get() :
+        r8 = 1
+    else:
+        r8 = 0
+
+def clickB9():
+    global r9
+    global checkB9
+    if checkB9.get() :
+        r9 = 1
+    else:
+        r9 = 0
+
+def clickB10():
+    global r10
+    global checkB10
+    if checkB10.get() :
+        r10 = 1
+    else:
+        r10 = 0
+
+def clickB11():
+    global r11
+    global checkB11
+    if checkB11.get() :
+        r11 = 1
+    else:
+        r10 = 0
+
+def clickB12():
+    global r12
+    global checkB12
+    if checkB12.get() :
+        r12 = 1
+    else:
+        r12 = 0
+
+def clickB13():
+    global r13
+    global checkB13
+    if checkB13.get() :
+        r13 = 1
+    else:
+        r13 = 0
+
+def clickB14():
+    global r14
+    global checkB14
+    if checkB14.get() :
+        r14 = 1
+    else:
+        r14 = 0
 
 #establishing the Window class and giving it an exit button
 class Window(Frame):
